5261ee5ca1334a7bc0ab2470256de32757a1c2e9
Roger Rezende <javielrezende@gmail.com>
2026-04-01 23:13:23 -0300

    feat: add Docker setup and PDF ingestion to PGVector with Gemini embeddings

--- src/ingest.py
+++ src/ingest.py
@@ -1,4 +1,9 @@
 import os
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from langchain_core.documents import Document
+from langchain_postgres import PGVector 
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -6,8 +11,43 @@ load_dotenv()
 PDF_PATH = os.getenv("PDF_PATH")
 
 def ingest_pdf():
-    pass
+    for k in ("GOOGLE_EMBEDDING_MODEL","DATABASE_URL","PG_VECTOR_COLLECTION_NAME"):
+        if not os.getenv(k):
+            raise RuntimeError(f"Environment variable {k} is not set")
 
+    if not PDF_PATH:
+        raise RuntimeError("Environment variable PDF_PATH is not set")
+    
+    pdf_path = os.path.join(PDF_PATH, "document.pdf")
+
+    docs = PyPDFLoader(str(pdf_path)).load()
+
+    splits = RecursiveCharacterTextSplitter(
+        chunk_size=1000, 
+        chunk_overlap=150, add_start_index=False).split_documents(docs)
+    if not splits:
+        raise SystemExit(0)
+
+    enriched = [
+        Document(
+            page_content=d.page_content,
+            metadata={k: v for k, v in d.metadata.items() if v not in ("", None)}
+        )
+        for d in splits
+    ]    
+
+    ids = [f"doc-{i}" for i in range(len(enriched))]
+
+    embeddings = GoogleGenerativeAIEmbeddings(model=os.getenv("GOOGLE_EMBEDDING_MODEL"))
+
+    store = PGVector(
+        embeddings=embeddings,
+        collection_name=os.getenv("PG_VECTOR_COLLECTION_NAME"),
+        connection=os.getenv("DATABASE_URL"),
+        use_jsonb=True,
+    )
+
+    store.add_documents(documents=enriched, ids=ids)
 
 if __name__ == "__main__":
     ingest_pdf()
